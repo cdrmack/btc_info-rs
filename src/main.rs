d49c460b8dfde116d1788642c3ae92015395da8f
cdrmack <git.pynvt@passmail.net>
2024-05-24 20:54:35 +0200

    initial GET request to the endpoint
    api-key is read from the env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv;
+use reqwest;
+use tokio;
+
+const ENDPOINT: &str = "https://btcbook.nownodes.io/api/";
+
+#[tokio::main]
+async fn main() {
+    let api_key = dotenv::var("API_KEY").expect("Cannot find API_KEY");
+    let client = reqwest::Client::new();
+
+    let response = client
+	.get(ENDPOINT)
+	.header("api-key", api_key)
+	.send()
+	.await
+	.expect("Failed to get response")
+	.text()
+	.await
+	.expect("Failed to convert payload");
+
+    println!("response = {:?}", response);
 }
