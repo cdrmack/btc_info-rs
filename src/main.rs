3e004896fdaaa230d8a042c242a4f8c8aa3333ec
cdrmack <git.pynvt@passmail.net>
2024-05-25 13:13:23 +0200

    deserialize BlockchainStatus

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
+#[macro_use]
+extern crate serde_derive;
+
 use dotenv;
 use reqwest;
 use tokio;
+use serde_json;
+
+mod blockchain_status;
+
+use crate::blockchain_status::BlockchainStatus;
 
 const ENDPOINT: &str = "https://btcbook.nownodes.io/api/";
 
 #[tokio::main]
-async fn main() {
+async fn send_request(url: &str) -> String {
     let api_key = dotenv::var("API_KEY").expect("Cannot find API_KEY");
     let client = reqwest::Client::new();
 
-    let response = client
-	.get(ENDPOINT)
+    client
+	.get(url)
 	.header("api-key", api_key)
 	.send()
 	.await
 	.expect("Failed to get response")
 	.text()
 	.await
-	.expect("Failed to convert payload");
+	.expect("Failed to convert payload")
+}
 
-    println!("response = {:?}", response);
+fn main() {
+    let response = send_request(&ENDPOINT);
+    let parsed: BlockchainStatus = serde_json::from_str(&response).expect("Cannot parse response (JSON)");
+    println!("QUERY {} (chain: {})", parsed.blockbook.coin, parsed.backend.chain);
 }
